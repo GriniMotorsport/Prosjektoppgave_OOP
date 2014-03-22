c09b2407729a12dd30cd6cd202d7affc3ca8fa27
sigvenss <sigve.net@gmail.com>
2014-03-22 12:15:58 +0100

    skrivmeny og main

--- Prosjekt_OOP/Prosjekt_OOP/main.cpp
+++ Prosjekt_OOP/Prosjekt_OOP/main.cpp
@@ -1,9 +1,55 @@
 #include <iostream>
+#include "SKRIVMENY.h"
+#include "KUNDE.h"
 using namespace std;
 
-int main() {
-  cout << "Test";
+int main()	{
+
+char kommando;
+
+skriv_meny1();
+kommando = menyValg();
+
+  while (kommando != 'Q')  {
+    switch(kommando)  {
+      case 'E': eiendom();
+		  skriv_meny2();
+kommando = menyValg();
+	
+  while (kommando != 'Q')  {
+    switch(kommando)  {
+      case 'D': eiendom_display();		break;    
+      case 'N': eiendom_ny();			break;    
+      case 'S': eiendom_slett();   		break;    
+      case 'E': eiendom_endre();		break;
+      default:  skriv_meny();			break;
+    }
+	kommando = menyValg();
+  }
+		break;   
+
+      case 'K': sonenr_display();	break;
+      case 'S': kunde();   			
+		  		  skriv_meny3();
+kommando = menyValg();
+  
+  while (kommando != 'Q')  {
+    switch(kommando)  {
+      case 'D': kunde_display();		break;    
+      case 'N': kunde_ny();		break;    
+      case 'S': kunde_slett();   		break;    
+      case 'E': kunde_endre();
+      default:  skriv_meny();		break;
+    }
+	kommando = menyValg();
+  }	  
+		  break; 
+      case 'U': utskrift();			break;   
+      default:  skriv_meny();		break;
     
-    cout<<"dere har gjørt bra jobb";
-    return 0;
-}
+	kommando = menyValg();
+	}
+
+  }
+
+  
