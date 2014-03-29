eb31241f48f84026337a10c0f3323109ee6cd701
GriniMotorsport <gudbrand.g@gmail.com>
2014-03-29 12:07:34 +0100

    Endringer med main
    Endringer for bruk og funksjon

--- Prosjekt_OOP/Prosjekt_OOP/GLOBALE.cpp
+++ Prosjekt_OOP/Prosjekt_OOP/GLOBALE.cpp
@@ -11,7 +11,7 @@ char meny_valg() {
 }
 
 void skriv_meny1()  {         //  Skriver meny/lovlige kommandoer til bruker:
-  cout << "\n\n\n Hva vil du gjøre?\n"
+  cout << "\n\n\n Hva vil du gjï¿½re?\n"
           "\tE - Eiendom\n"
           "\tS - Skriv ut data om alle eiendommene/oppdragene i en sone.\n"
 		  "\tK - Kunde\n"
@@ -19,24 +19,67 @@ void skriv_meny1()  {         //  Skriver meny/lovlige kommandoer til bruker:
 		  "\tQ - Quit / avslutt\n";
 }
 
-void eiendom() {
+void eiendom() { // ------------------ eiendom() ------------------------------
   char kommando;
 	skriv_meny2();
 
 	kommando = meny_valg();
 
 	while (kommando != 'Q') {
-		switch (switch_on)
-		{
-		default:
-			break;
+		switch (kommando){
+		case 'D' : cout << "ED"; break; 
+		case 'N' : cout << "EN"; break; 
+		case 'S' : cout << "ES"; break; 
+		case 'E' : cout << "EE"; break; 
+		default:   cout << "DEFAULT-E"; break;
 		}
 	}
-	{
+} // -------------------------------------------------------------------------
 
+void kunde() { // ---------------- kunde() ------------------------------------
+  char kommando;
+	skriv_meny3();
+
+	kommando = meny_valg();
+
+	while (kommando != 'Q') {
+		switch (kommando){
+		case 'D' : cout << "KD"; break; 
+		case 'N' : cout << "KN"; break; 
+		case 'S' : cout << "KS"; break; 
+		case 'E' : cout << "KE"; break; 
+		default:   cout << "DEFAULT-K"; break;
+		}
 	}
-}
+} // ------------------------------------------------------------------------
 
+void sone() { // ---------------- sone() ------------------------------------
+  char kommando;
+	skriv_meny3();
+
+	kommando = meny_valg();
+
+	while (kommando != 'Q') {
+		switch (kommando){
+		case 'D' : cout << "SD"; break; 
+		default:   cout << "DEFAULT-S"; break;
+		}
+	}
+} // ------------------------------------------------------------------------
+
+void status() { // ---------------- sone() ------------------------------------
+  char kommando;
+	skriv_meny3();
+
+	kommando = meny_valg();
+
+	while (kommando != 'Q') {
+		switch (kommando){
+		case 'D' : cout << "US"; break; 
+		default:   cout << "DEFAULT-S"; break;
+		}
+	}
+} // ------------------------------------------------------------------------
 
 void skriv_meny2()  {         //  Skriver meny/lovlige kommandoer til bruker:
   cout << "\n\n\n\t----------- Eiendom -----------\n"
@@ -49,7 +92,7 @@ void skriv_meny2()  {         //  Skriver meny/lovlige kommandoer til bruker:
 
 
 void skriv_meny3()  {         //  Skriver meny/lovlige kommandoer til bruker:
-  cout << "\n\n\nHva vil du gjøre?\n";
+  cout << "\n\n\nHva vil du gjï¿½re?\n";
   cout << "\tD - Display en kunde\n";
   cout << "\tN - Opprett en ny kunde\n";
   cout << "\tS - Slett en kunde\n";
