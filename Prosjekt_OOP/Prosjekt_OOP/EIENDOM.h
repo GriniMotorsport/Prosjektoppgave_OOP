3eec2ae7843866f5623022fc5b12d98ec8bdb106
Maryam <maryam.mahjoob@gmail.com>
2014-03-29 10:57:38 +0100

    Endring i eiendom
    lagt til classe

--- Prosjekt_OOP/Prosjekt_OOP/EIENDOM.h
+++ Prosjekt_OOP/Prosjekt_OOP/EIENDOM.h
@@ -1,5 +1,92 @@
 #if !defined(__EIENDOM_h)
 #define  __EIENDOM_h
+class Kunder {
+private:
+	
+	int forstN_K;					//forste haavverende kunde
+	int sisti_K;					//sist innlagte kunde
+	List Kunde;						//datastrukture med alle kunde
+
+public:
+
+}
+
+class Kunde{
+private:
+
+	int tlf; //telephone numere
+		char* navn; //navn av kunde
+		char* adr; // gata adress
+		char* post_adr; //post adress
+		char* Mail; //mail
+		List Intrsone; //Intrsone
+public:
+
+}
+
+class Intersone{
+private:
+	int max_pris;
+	int min_areal;
+	int minA_rom;
+	int tilsen_over;
+	int interessert;
+	int eien_t;
+public:
+
+}
+
+class Soner{
+private:
+	int sist_eindom; //sist innlagte eiendom
+	array sone [];//namber av brukers
+
+public:
+
+}
+class sone{
+private:
+char* info; //beskrivelse av sonen
+List Eiendomer; //Eiendom til salg leie i sonen
+public:
+
+}
+
+class Eiendom{
+private:
+	int datoInn;  //dato innlagt i systemet
+	int bruk_num; //number av brukers
+	int pris; //pris
+	int areal;  // tomtens areal i kvardratmeter
+	char* adr_num; //gata adresse
+	char* post_adr; // post adress
+	char* nam;//næværende eier navn
+	int eiendomstype; //eiendomstype
+public:
+
+}
+
+class Bolig public :: Eiendom {
+private:
+	int byg_aar; // byggeår
+	int bruttoareal; //bruttoareal
+	int boareal;//boareal
+	int ant_sovrom;//antal soverom
+	int interesert;//
+public:
+
+}
+
+class Bolig {
+private:
+public:
+}
+
+
+
+
+
+
 
 void eiendom_display();
 void eiendom_ny();
