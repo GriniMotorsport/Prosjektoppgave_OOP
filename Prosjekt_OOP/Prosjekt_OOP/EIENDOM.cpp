53a6505fecaa08965a23598a82865e626c1c33f7
sigvenss <sigve.net@gmail.com>
2014-04-05 19:48:59 +0200

    sigve

--- Prosjekt_OOP/Prosjekt_OOP/EIENDOM.cpp
+++ Prosjekt_OOP/Prosjekt_OOP/EIENDOM.cpp
@@ -11,6 +11,29 @@
 using namespace std;
 
 void Eiendom::display()	{
+	cout << "\nDato for opprettelse av tomten/eiendom:	" << dato;
+	cout << "\nBruks-nummer:	" << bruksNr;
+	cout << "\nSaksbehandlers nr:	" << saksB;
+	cout << "\nPris for tomten/eiendomen:	" << pris;
+	cout << "\nAreal på tomten/eiendomen:	" << areal;
+	cout << "\nGateadresse:	" << gateadresse;
+	cout << "\nPostadresse:	" << postAdresse;
+	cout << "\nEiers navn:	" << eiersN;
+	cout << "\nKommune navn:	" << kommune;
+	cout << "\nBeskrivelse:	"	<< info << endl;
 
 
 }
+
+
+
+
+
+	int areal;				// Areal på tomten/eiendomen
+	char* gateadresse;		// Gateadresse (gate + nr)
+	char* postAdresse;		// Postadresse (nr + sted)
+	char* eiersN;			// Eiers navn
+	char* kommune;			// Kommune navn
+	char* info;				// Beskrivelse av eiendomen
+	Eiendomstype type;		// Enum
+	Boliginfo info;			// Enum
